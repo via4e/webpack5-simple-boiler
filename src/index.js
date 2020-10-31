71ee5b1ced2ab9c1f1e90154e405769dc2826dc7
Viacheslaw Saveliew <version-1@yandex.ru>
2020-10-31 20:25:56 +0300

    eslint added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // img
-import examplePNG from './img/test64x64.png'
-import exampleSVG from './img/test.svg'
+const examplePNG = require('./img/test64x64.png')
+const exampleSVG = require('./img/test.svg')
 
 //styles
 import './styles/main.scss'
@@ -12,3 +12,18 @@ heading.textContent = 'Как интересно!'
 // добавляем заголовок в DOM
 const root = document.querySelector('#app')
 root.append(heading)
+
+//img
+
+const image1 = document.createElement('img')
+image1.src = examplePNG
+image1.setAttribute('width', 211)
+image1.setAttribute('height', 211)
+root.append(image1)
+
+
+const imageSVG = document.createElement('img')
+imageSVG.src = exampleSVG
+imageSVG.setAttribute('width', 200)
+imageSVG.setAttribute('height', 200)
+root.append(imageSVG)
